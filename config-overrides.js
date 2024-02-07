be788a76954469fa8e0c15e653a65d333dfe2052
啊豪 <1009183116@qq.com>
2024-02-07 17:45:21 +0800

    fix: 视频窗口

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+// const { override } = require("customize-cra");
+
+// module.exports = override(
+//     (config, env) => {
+//         //修改target，告诉webpack我们在用electron，webpack会做些特殊处理，不然我们引入electron模块时，会解析不了。
+//         //或者使用window.require来解决
+//         // config.target = "electron-renderer";     
+//         return config;
+//     }
+// );
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSelect = () => {
+    const { ipcRenderer } = window.require('electron');
+    ipcRenderer.send('send-video');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSelect}>选择图片</button>
     </div>
   );
 }
